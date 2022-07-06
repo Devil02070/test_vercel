ad71ce94e448475a435163360f557a99f15c28e0
Ajay Kumar <devilajay1090@gmail.com>
2022-07-06 16:55:08 +0530

    save

--- source/db/conn.js
+++ source/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/unicorn', {
-// mongoose.connect('', {
+// mongoose.connect('mongodb://localhost:27017/unicorn', {
+mongoose.connect('mongodb+srv://Ajay1090:hTMr2f7LuJCMeuZA@cluster0.apxowbx.mongodb.net/unicornsoul?retryWrites=true&w=majority', {
     useNewUrlParser:true,
     useUnifiedTopology: true
     // useCreateIndex:true
