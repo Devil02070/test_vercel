d86ef2593529a929da5ae974fa00b4a29cfd7cb0
Ajay Kumar <devilajay1090@gmail.com>
2022-07-06 12:12:37 +0530

    save

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
+const path = require("path");
+const hbs = require("hbs");
+
+const static_path = path.join(__dirname, './public');
+const temp_path = path.join(__dirname, './templates/views');
+const partials = path.join(__dirname, './templates/partials');
+
+console.log(static_path);
+console.log(temp_path);
+console.log(partials);
+
+app.use(express.static(static_path));
+app.set('view engine', 'hbs');
+app.set('views', temp_path);
+hbs.registerPartials(partials);
+
+
 app.get("/",(req,res)=>{
     res.send("<h1>Bhai sahab kidaaan??</h1>");
 })
