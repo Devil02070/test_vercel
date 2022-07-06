7189cae587a76ff60249f1584770385498df7571
Ajay Kumar <devilajay1090@gmail.com>
2022-07-06 15:41:03 +0530

    save

--- source/db/conn.js
+++ source/db/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/unicorn', {
+// mongoose.connect('mongodb://localhost:27017/unicorn', {
+mongoose.connect('mongodb+srv://vercel-admin-user:sVYsKN9LVY7tduxz@cluster0.sl4lsln.mongodb.net/admin', {
     useNewUrlParser:true,
     useUnifiedTopology: true
     // useCreateIndex:true
