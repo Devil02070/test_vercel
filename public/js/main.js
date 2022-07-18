945b581fb5dc90a24edfc027be11f33d5f3dc0d6
Ajay Kumar <devilajay1090@gmail.com>
2022-07-18 09:43:21 +0530

    save

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,15 @@
 console.log('hii');
+function user_details(){
+    console.log('coming');
+    // document.getElementsByClassName('user-details').style.css = 'display: none';
+    document.getElementById('dt-user').style.css = 'display: none';
+}
 function user_orders(){
     console.log('coming');
     document.getElementsByClassName('user-orders').style.cssText = 'display: none';
 }
-$(document).ready(function(){
-    console.log('hii');
-})
+function user_history(){
+    console.log('coming');
+    document.getElementsByClassName('user-history').style.cssText = 'display: none';
+}
 
